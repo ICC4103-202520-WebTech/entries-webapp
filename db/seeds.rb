70c307c9de7af268c234ac2ae938d41d612e9617
Andrés Howard <ahowardm@gmail.com>
2025-10-01 09:56:45 -0300

    views for entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Entry.delete_all
+
+Entry.create! title: "Reflexión 1", body: "Este es el contenido de la reflexión 1"
+Entry.create! title: "Reflexión 2", body: "Este es el contenido de la reflexión 2"
+Entry.create! title: "Reflexión 3", body: "Este es el contenido de la reflexión 3"
+Entry.create! title: "Reflexión 4", body: "Este es el contenido de la reflexión 4"
+Entry.create! title: "Reflexión 5", body: "Este es el contenido de la reflexión 5"
